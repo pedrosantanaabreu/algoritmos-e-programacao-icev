cf8b68feda5a5adefa2e062a874c7d6abc44995f
Pedro Santana Abreu <pedro.santana-professional@outlook.com>
2022-04-18 23:27:01 -0300

    update exercicio 1 - test

---
+++ algoritmos-e-programacao-icev/exercicios/18-04-2022/python/exercicio-1.py
@@ -0,0 +1,79 @@
+'''
+@Pedro Santana Abreu (https://linktr.ee/pedrosantanaabreu)
+@Icev (https://somosicev.com)
+
+PT-BR:
+Uma empresa decidiu fazer um levantamento em relação aos
+candidatos que se apresentarem para preenchimento de vagas em seu
+quadro de funcionários. Supondo que você seja o programador dessa
+empresa, faça um programa que leia a quantidade de candidatos que se
+apresentaram e, para cada um, leia a idade, o sexo (M ou F) e a
+experiência no serviço (S ou N). O programa deve calcular e mostrar:
+
+a) o número de candidatos do sexo feminino;
+
+b) o número de candidatos do sexo masculino;
+
+c) a idade média dos homens que já têm experiência no serviço;
+
+d) a porcentagem dos homens com mais de 45 anos entre o total dos
+homens;
+
+e) o número de mulheres com idade inferior a 21 anos e com experiência
+no serviço;
+
+f) a menor idade entre as mulheres que já têm experiência no serviço.
+'''
+
+quantidade_feminino = 0
+feminino_menor_21_anos_com_experiencia = 0
+feminino_menor_idade_experiencia = 0
+
+quantidade_masculino = 0
+soma_idade_masculino_experiencia = 0
+quantidade_masculino_experiencia = 0
+masculino_maior_45_anos = 0
+porcentagem_masculino_45_anos = 0
+idade_media_masculino_experiencia= 0
+
+numero_candidatos = int(input('Digite o número de candidatos | '))
+for i in range(0, numero_candidatos):
+    idade = int(input(f'\nDigite a idade do {i + 1}º candidato | ')) 
+    sexo = input(f'Digite o sexo do {i + 1}º candidato (M / F) | ')
+    experiencia_servico = input(f'Digite o se o {i + 1}º candidato tem experiência no serviço (S ou N) | ')
+
+    if sexo == 'M':
+        quantidade_masculino += 1
+
+        if idade > 45:
+            masculino_maior_45_anos += 1
+
+        if experiencia_servico == 'S':
+            quantidade_masculino_experiencia += 1
+            soma_idade_masculino_experiencia += idade
+    else:
+        quantidade_feminino += 1
+
+        if idade < 21 and experiencia_servico == 'S':
+            if idade < feminino_menor_idade_experiencia:
+                feminino_menor_idade_experiencia = idade
+            else:
+                feminino_menor_idade_experiencia = idade
+
+            feminino_menor_21_anos_com_experiencia += 1
+        
+if quantidade_masculino_experiencia != 0:
+    idade_media_masculino_experiencia = soma_idade_masculino_experiencia / quantidade_masculino_experiencia
+
+if masculino_maior_45_anos != 0:
+    porcentagem_masculino_45_anos = quantidade_masculino / masculino_maior_45_anos
+
+print(f'''
+O número de candidatos do sexo feminino | {quantidade_feminino}
+O número de candidatos do sexo masculino | {quantidade_masculino}
+
+Idade média dos homens que já têm experiência no serviço | {idade_media_masculino_experiencia:.2f}
+Porcentagem dos homens com mais de 45 anos entre o total dos homens | {porcentagem_masculino_45_anos:.2f} %
+Número de mulheres com idade inferior a 21 anos e com experiência no serviço | {feminino_menor_21_anos_com_experiencia}
+Menor idade entre as mulheres que já têm experiência no serviço | {feminino_menor_idade_experiencia}
+''')
