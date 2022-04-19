3c97e2758dfec578a17fe047e2ed6fc1583a4e19
Pedro Santana Abreu <pedro.santana-professional@outlook.com>
2022-04-19 00:34:29 -0300

    exercicio 1 update 4

--- algoritmos-e-programacao-icev/exercicios/18-04-2022/python/exercicio-1.py
+++ algoritmos-e-programacao-icev/exercicios/18-04-2022/python/exercicio-1.py
@@ -53,13 +53,14 @@ for i in range(1, numero_candidatos + 1):
         quantidade_masculino += 1
         if idade > 45:
             masculino_maior_45_anos += 1
-            porcentagem_masculino_45_anos = quantidade_masculino / masculino_maior_45_anos
 
         if experiencia_servico == 'S':
             quantidade_masculino_experiencia += 1
             soma_idade_masculino_experiencia += idade
             idade_media_masculino_experiencia = soma_idade_masculino_experiencia / quantidade_masculino_experiencia
-            
+    
+        porcentagem_masculino_45_anos =  masculino_maior_45_anos / quantidade_masculino
+
     else:
         quantidade_feminino += 1
         if experiencia_servico == 'S':
@@ -78,7 +79,7 @@ O número de candidatos do sexo feminino | {quantidade_feminino}
 O número de candidatos do sexo masculino | {quantidade_masculino}
 
 Idade média dos homens que já têm experiência no serviço | {idade_media_masculino_experiencia:.2f}
-Porcentagem dos homens com mais de 45 anos entre o total dos homens | {porcentagem_masculino_45_anos:.2f} %
+Porcentagem dos homens com mais de 45 anos entre o total dos homens | {porcentagem_masculino_45_anos:.2%}
 Número de mulheres com idade inferior a 21 anos e com experiência no serviço | {feminino_menor_21_anos_com_experiencia}
 Menor idade entre as mulheres que já têm experiência no serviço | {feminino_menor_idade_experiencia} anos
 ''')
