904ee9f91e8fd4beade9dc2b83c677fdc1988c01
Pedro Santana Abreu <90485116+pedrosantanaabreu@users.noreply.github.com>
2022-09-29 01:43:32 -0300

    Update exercicio-1.py

--- algoritmos-e-programacao-icev/exercicios/28-09-2022/exercicio-1.py
+++ algoritmos-e-programacao-icev/exercicios/28-09-2022/exercicio-1.py
@@ -3,4 +3,4 @@
 ser passado na linha de comando na execução do programa:
 Ex: python imprimir_arquivo arquivo.txt
 """
-def imprimir_arquivo(): [print(linha.removesuffix('\n')) for linha in open(input('[ - ] Insira o comando: python imprimir_arquivo exemplo.txt\n[ > ] ').strip().lower().replace('python imprimir_arquivo ', ''), 'r').readlines()]
+def imprimir_arquivo(): imprimir_arquivo = {'count' : 1, 'import' : lambda: exec('import os') or exec("os.system('cls||clear')"), 'inicial' : lambda: imprimir_arquivo['import']() or [[imprimir_arquivo['import']() or print(f'[ - ] Resultado\n{linha}', end='') or imprimir_arquivo.pop('count')] if imprimir_arquivo.get('count', 'segundo') != 'segundo' else print(linha.removesuffix('\n')) for linha in open(input('[ - ] Insira o comando: python imprimir_arquivo exemplo.txt\n[ > ] ').strip().lower().replace('python imprimir_arquivo ', ''), 'r' ).readlines()], 'enter' : lambda: input('[ - ] Digite "Enter" para continuar...')}; imprimir_arquivo['inicial']() and imprimir_arquivo['enter']()
