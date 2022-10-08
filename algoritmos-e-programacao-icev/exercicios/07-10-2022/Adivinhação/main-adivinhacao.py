fcaa13741625915ccdc66d3312beee9175288292
Pedro Santana Abreu <90485116+pedrosantanaabreu@users.noreply.github.com>
2022-10-08 20:05:34 -0300

    Update main-adivinhacao.py

--- algoritmos-e-programacao-icev/exercicios/07-10-2022/Adivinhação/main-adivinhacao.py
+++ algoritmos-e-programacao-icev/exercicios/07-10-2022/Adivinhação/main-adivinhacao.py
@@ -1,3 +1,12 @@
+"""
+Desenvolva um jogo de acerte o número, onde o computador escolhe um número inteiro aleatório de 0 a 100, inicialmente, e o usuário tem 5 tentativas para adivinhar o número
+OBS.: O design da tela pode ser implementado livremente
+Forneça dica se um número informado está no intervalo maior ou menor.
+Dica: Use a import random
+Implemente um sistema de pontuação com o seguinte comportamento: se o usuário adivinhar o número na primeira tentativa, receberá a pontuação máxima (ex. 100 pontos); se o usuário adivinhar o número na última tentativa, receberá a pontuação mínima (ex. 10 pontos); se o usuário não acertar o número, não receberá nenhum ponto.
+Implemente um controle de erros. Caso o jogador digite um número fora da faixa permitida ou caracteres não numéricos, o sistema deve notificar o jogador e solicitar o input correto.
+Implemente a opção de o usuário iniciar uma nova partida. Ao finalizar uma rodada, após o resultado final, o jogo deve perguntar se o jogador quer iniciar uma nova partida e, em caso negativo, encerrar a aplicação.
+"""
 from adivinhacao import JogoAdivinhacao
 
 # Configurações iniciais
