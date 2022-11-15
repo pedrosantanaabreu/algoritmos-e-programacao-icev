2447f04ae6bb9f66a7f5570d73292260dcdc49a4
Pedro Santana Abreu <90485116+pedrosantanaabreu@users.noreply.github.com>
2022-11-15 15:46:50 -0300

    Update app.py

--- algoritmos-e-programacao-icev/exercicios/outros-projetos/banco/sistema/app.py
+++ algoritmos-e-programacao-icev/exercicios/outros-projetos/banco/sistema/app.py
@@ -1,11 +1,14 @@
 if __name__ == '__main__':
-    from src import Setup, Main
+    try:
+        from src import Setup, Main
 
 
-    class App:
-        def __init__(self):
-            Setup.iniciar_instalacao()
-            Main.iniciar_aplicacao()
+        class App:
+            def __init__(self):
+                Setup.iniciar_instalacao()
+                Main.iniciar_aplicacao()
 
 
-    App()
+        App()
+    except:
+        pass
