4f71487f8fc613185fbab4031c0cce83561faa58
Pedro Santana Abreu <90485116+pedrosantanaabreu@users.noreply.github.com>
2022-11-16 00:29:38 -0300

    Update app.py

--- algoritmos-e-programacao-icev/exercicios/outros-projetos/sistema-bancario/sistema/app.py
+++ algoritmos-e-programacao-icev/exercicios/outros-projetos/sistema-bancario/sistema/app.py
@@ -8,21 +8,22 @@ Sistema de gerenciamento de contas
 
 
 # Módulos internos
-from src import Main
 from src import Setup
 
 
 if __name__ == "__main__":
     try:
-        from src import Setup, Main
-
         class App:
             def __init__(self) -> None:
                 # Verificando todos os arquivos e pendências
                 Setup.iniciar_instalacao()
-                
+
                 # Iniciando aplicação
+                from src import Main
+
+
                 Main.iniciar_aplicacao()
+        
         
         # Iniciando aplicação
         App()
